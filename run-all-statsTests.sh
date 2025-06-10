a7d1161ba41d1264449cfc9f05634f96f65d908d
Daniel R. Reynolds <reynolds@smu.edu>
2025-06-10 01:41:02 -0500

    python -> python3

--- run-all-statsTests.sh
+++ run-all-statsTests.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 #
-# This script generates excel files and text files for all tests (stiff Brusselator and KPR), controllers and MRI methods. 
+# This script generates excel files and text files for all tests (stiff Brusselator and KPR), controllers and MRI methods.
 
 echo -e "Running repeated measures ANOVA for all MRI methods of a specific order\n"
-python runStats_repeatedM_ANOVA.py
+python3 runStats_repeatedM_ANOVA.py
 
 echo -e "Running One-Way ANOVA for all controllers\n"
-python runStats_oneWayANOVA_ctrl.py
+python3 runStats_oneWayANOVA_ctrl.py
 
 echo -e "Running statistical analysis for Brusselator and KPR, for all MRI methods with a fixed controller \n"
-python runStats_fixedCtrl_tests.py
+python3 runStats_fixedCtrl_tests.py
 
 echo -e "Running statistical analysis for Brusselator and KPR, for all controllers with a fixed MRI method \n"
-python runStats_fixedMethod_tests.py
+python3 runStats_fixedMethod_tests.py
 
 echo -e "Running statistical analysis for all MRI methods with a fixed controller, independent of test problem\n"
-python runStats_fixedCtrl_Indep_tests.py
+python3 runStats_fixedCtrl_Indep_tests.py
 
 echo -e "Running statistical analysis for all controllers with a fixed MRI method, independent of test problem\n"
-python runStats_fixedMethod_Indep_tests.py
+python3 runStats_fixedMethod_Indep_tests.py
 
 echo -e "Running statistical analysis for Brusselator and KPR, for all controllers \n"
-python runStats_allCtrls.py
+python3 runStats_allCtrls.py
 
 echo -e "Running statistical analysis for Brusselator and KPR, for all HTol and Decoupled controllers \n"
-python runStats_hTol_dec_ctrl.py
+python3 runStats_hTol_dec_ctrl.py
 
 echo -e "Run tests completed!\n"
